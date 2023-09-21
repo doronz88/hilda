c1281bb7022260a851804808212bcb3897b87c31
netanelc305 <netanelc305@pm.me>
2023-09-21 08:54:49 +0300

    wip

--- hilda/snippets/boringssl.py
+++ hilda/snippets/boringssl.py
@@ -3,10 +3,22 @@ import lldb
 _FILENAME = '/tmp/hilda-keylog.txt'
 
 
-def _ssl_log_secret_bp(hilda, *args):
+def _extract_params_arm(hilda):
     label = hilda.registers.x1.peek_str()
     secret = hilda.registers.x2.peek(hilda.registers.x3).hex()
     random = (hilda.registers.x0[6] + 48).peek(32).hex()
+    return label, secret, random
+
+
+def _extract_params_intel(hilda):
+    label = hilda.registers.rsi.peek_str()
+    secret = hilda.registers.rdx.peek(hilda.registers.rcx).hex()
+    random = (hilda.registers.rdi[6] + 48).peek(32).hex()
+    return label, secret, random
+
+
+def _ssl_log_secret_bp(hilda, *args):
+    label, secret, random = _extract_params_intel(hilda) if hilda.arch == 'x86_64h' else _extract_params_arm(hilda)
     print(f'ssl_log_secret\n'
           f'    label: {label}\n'
           f'    secret: {secret}\n'

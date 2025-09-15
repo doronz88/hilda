773d8adcf3220997609b1d6db2b4ed1591637a70
DoronZ <doron88@gmail.com>
2025-09-15 22:03:59 +0300

    tests: Run tests on `remoted`

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import pytest
 from hilda.hilda_client import HildaClient
 from hilda.launch_lldb import create_hilda_client_using_attach_by_name
 
-PROCESS = 'awdd'
+PROCESS = 'remoted'
 
 
 @pytest.fixture(scope='function')
